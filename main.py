196e20eaef5b7e3770cefd1559028a0ac6ceec3b
Sh1v9m <142924443+Sh1v9m@users.noreply.github.com>
2024-08-04 17:29:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template_string app = Flask(__name__)
+from flask import Flask, request, render_template_string 
+app = Flask(__name__)
 # HTML template for the form
 form_template = '''
 <!DOCTYPE html>
